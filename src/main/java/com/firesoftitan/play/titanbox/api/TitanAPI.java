199bbcca54f7b80b8d4cde79b2a53771c64e9a37
freethemice <cgfreethemice@gmail.com>
2022-02-25 15:23:30 -0600

    Initial commit

---
+++ src/main/java/com/firesoftitan/play/titanbox/api/TitanAPI.java
@@ -0,0 +1,34 @@
+package com.firesoftitan.play.titanbox.api;
+import com.firesoftitan.play.titanbox.api.listeners.MainListener;
+import com.firesoftitan.play.titanbox.libs.tools.LibsMessageTool;
+import com.firesoftitan.play.titanbox.libs.tools.Tools;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.logging.Level;
+
+public final class TitanAPI extends JavaPlugin {
+
+    public static TitanAPI instants;
+    public static MainListener mainListener;
+    public static Tools tools;
+    public static LibsMessageTool messageTool;
+    @Override
+    public void onEnable() {
+        instants = this;
+        tools = new Tools(this);
+        mainListener = new MainListener();
+        mainListener.registerEvents();
+        messageTool = tools.getMessageTool();
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            messageTool.sendMessageSystem("Could not find PlaceholderAPI! This plugin is required.", Level.WARNING);
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/com/firesoftitan/play/titanbox/api/listeners/MainListener.java
@@ -0,0 +1,76 @@
+package com.firesoftitan.play.titanbox.api.listeners;
+
+
+import com.firesoftitan.play.titanbox.api.TitanAPI;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.event.EventException;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class MainListener implements Listener {
+
+    private static HashMap<UUID, Location> changeNames = new HashMap<UUID, Location>();
+    private static HashMap<UUID, Location> changeIcons = new HashMap<UUID, Location>();
+    public MainListener(){
+
+    }
+    public void registerEvents(){
+        PluginManager pm = TitanAPI.instants.getServer().getPluginManager();
+        pm.registerEvents(this, TitanAPI.instants);
+    }
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
+    public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event){
+        if (event.getPlayer().hasPermission("titanbox.api") || event.getPlayer().isOp()) {
+            String message = event.getMessage();
+            message = PlaceholderAPI.setPlaceholders(event.getPlayer(), message);
+            event.setMessage(message);
+        }
+    }
+    List<RegisteredListener> commandListers = new ArrayList<RegisteredListener>();
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){
+        try {
+            RegisteredListener[] allRL = event.getHandlers().getRegisteredListeners();
+            for(int i = 0; i < allRL.length; i++)
+            {
+                if (allRL[i].getPlugin() != TitanAPI.instants)
+                {
+                    commandListers.add(allRL[i]);
+                    event.getHandlers().unregister(allRL[i]);
+                }
+            }
+            if (event.isCancelled()) return;
+            if (event.getPlayer().hasPermission("titanbox.api") || event.getPlayer().isOp()) {
+                String message = event.getMessage();
+                message = PlaceholderAPI.setPlaceholders(event.getPlayer(), message);
+                event.setMessage(message);
+            }
+            callUnregEvetns(event);
+        } catch (IllegalArgumentException e) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "Server has no commands =(");
+        }
+    }
+    private void callUnregEvetns(PlayerCommandPreprocessEvent event) {
+        for (int i = 0; i < commandListers.size(); i++)
+        {
+            try {
+                commandListers.get(i).callEvent(event);
+            } catch (EventException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
